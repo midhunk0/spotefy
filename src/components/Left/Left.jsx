8e8af99195f89a3f880fa42532bf9e4388c139ab
midhunrajk <midhunrajk009@gmail.com>
2023-07-25 08:28:23 +0530

    left shortening

--- src/components/Left/Left.jsx
+++ src/components/Left/Left.jsx
@@ -46,6 +46,16 @@ const Left=({ toggleLeftWidth, toggleMaxWidth })=>{
             </Link>
         )
     }
+
+    const SmallHeaderButtons=({link, icon})=>{
+        return(
+            <Link href={link} color="#000" sx={{textDecoration:"none"}}>
+                <Box display="flex" margin="8px" width="25px" padding="10px" height="25px">
+                    {icon}
+                </Box>
+            </Link>
+        )
+    }
     
     const LibraryButtons=({title})=>{
         return(
@@ -56,7 +66,51 @@ const Left=({ toggleLeftWidth, toggleMaxWidth })=>{
             </Button>
         )
     }
+
+    const Image=({src, alt})=>{
+        return(
+            <img 
+                src={src}
+                width="56px" 
+                height="56px" 
+                style={{ 
+                    borderRadius: "10px" 
+                }} 
+                alt={alt} 
+            />
+        )
+    }
     
+    const MaxTitle=({title, width, align})=>{
+        return(
+            <Typography fontSize="14px" fontWeight="bold" margin="8px" width={width} align={align}>
+                {title}
+            </Typography>
+        )
+    }
+
+    const mainBox={
+        display:"flex", 
+        flexDirection:"column", 
+        margin:"8px 0 8px 8px", 
+        gap:"8px"
+    }
+
+    const homeBox={
+        display:"flex", 
+        flexDirection:"column", 
+        bgcolor:"white", 
+        borderRadius:"10px"
+    }
+
+    const libraryBox={
+        display:"flex", 
+        flexDirection:"column", 
+        bgcolor:"white", 
+        borderRadius:"10px", 
+        overflow:"auto"
+    }
+
     const buttonStyle={
         borderRadius:"20px",
         background:"lightgray", 
@@ -71,6 +125,14 @@ const Left=({ toggleLeftWidth, toggleMaxWidth })=>{
         }
     }
 
+    const list={
+        display:"flex",
+        flexDirection:"column", 
+        position:"sticky", 
+        top:"0", 
+        zIndex:"1", 
+        bgcolor:"white"
+    }
 
     const listButton={
         ...buttonStyle,
@@ -83,18 +145,19 @@ const Left=({ toggleLeftWidth, toggleMaxWidth })=>{
         justifyContent: "left", 
     }
 
+
     return (
         <>
         {left ? (
-            <Box display="flex" flexDirection="column" margin="8px 0 8px 8px" gap="8px">
-                <Box display="flex" flexDirection="column" bgcolor="white" borderRadius="10px">
+            <Box sx={mainBox}>
+                <Box sx={homeBox}>
                     <HeaderButtons link="home" icon={<HiHome size="25px"/>} title="Home"/>
                     <HeaderButtons link="search" icon={<HiSearch size="25px"/>} title="Search"/>
                 </Box>
 
-                <Box display="flex" flexDirection="column" bgcolor="white" borderRadius="10px" overflow="auto">
-                    <Box display="flex" justifyContent="space-between">
-                        <Box margin="8px">
+                <Box sx={libraryBox}>
+                    <Box display="flex" margin="8px" justifyContent="space-between">
+                        <Box>
                             <ButtonBase disableRipple onClick={leftToggle}>
                                 <Box width="25px" height="25px" padding="10px">
                                     <HiCollection size="25px" color="black"/>
@@ -104,7 +167,7 @@ const Left=({ toggleLeftWidth, toggleMaxWidth })=>{
                                 </Typography>
                             </ButtonBase>
                         </Box>
-                        <Box display="flex" margin="8px" gap="8px" alignItems="center">
+                        <Box display="flex" gap="8px" alignItems="center">
                             <IconButton>
                                 <HiPlus size="20px" color="black"/>
                             </IconButton>
@@ -142,8 +205,8 @@ const Left=({ toggleLeftWidth, toggleMaxWidth })=>{
                             </>
                         ) : (
                             <Box display="flex" height="37px" gap="8px"> 
-                                    <LibraryButtons title="Playlists"/>
-                                    <LibraryButtons title="Artists"/>
+                                <LibraryButtons title="Playlists"/>
+                                <LibraryButtons title="Artists"/>
                             </Box>
                         )}
                     </Box>
@@ -151,23 +214,17 @@ const Left=({ toggleLeftWidth, toggleMaxWidth })=>{
                     <Box overflow="auto">
                         <List disablePadding>
                             {max ? (
-                                <Box display="flex" flexDirection="column" position="sticky" top={0} zIndex={1} bgcolor="white">
+                                <Box sx={list}>
                                     <Box display="flex">
-                                        <Typography fontSize="14px" fontWeight="bold" margin="8px" width="40%" align="left">
-                                            Title
-                                        </Typography>
-                                        <Typography fontSize="14px" fontWeight="bold" margin="8px" width="40%" align="center">
-                                            Date Added
-                                        </Typography>
-                                        <Typography fontSize="14px" fontWeight="bold" margin="8px" width="20%" align="right">
-                                            Played
-                                        </Typography>
+                                        <MaxTitle title="Title" width="40%" align="left"/>
+                                        <MaxTitle title="Date Added" width="40%" align="center"/>
+                                        <MaxTitle title="Played" width="20%" align="right"/>
                                     </Box>
                                     <hr style={{ color: "gray", width: "calc(100% - 16px)" }}>
                                     </hr>
                                 </Box>
                             ) : (
-                                <Box display="flex" flexDirection="column" position="sticky" top={0} zIndex={1} bgcolor="white">
+                                <Box sx={list}>
                                     <Box display="flex" margin="8px" justifyContent="space-between">
                                         <ButtonBase disableRipple sx={{background:"lightgray", borderRadius:"20px", margin:"8px"}}>
                                             <IconButton type="button" sx={{p:1, transition: "ease-in-out"}} onClick={toggleSearch}>
@@ -193,9 +250,9 @@ const Left=({ toggleLeftWidth, toggleMaxWidth })=>{
                             {data.map((item, index) => (
                                 <Button key={index} disableRipple sx={listButton} >
                                     {index === 1 ? (
-                                        <img src={`../../assets/olivia.jpeg`} width="56px" height="56px" style={{ objectFit: 'cover', borderRadius: "10px" }} alt="olivia" />
+                                        <Image src="../../assets/olivia.jpeg" alt="olivia" />
                                     ) : (
-                                        <img src={`../../assets/liked.png`} width="56px" height="56px" style={{ borderRadius: "10px" }} alt="liked" />
+                                        <Image src="../../assets/liked.png" alt="liked" />
                                     )}
                                     <Box alignItems="center" marginLeft="8px">
                                         {item}
@@ -207,23 +264,13 @@ const Left=({ toggleLeftWidth, toggleMaxWidth })=>{
                 </Box>
             </Box>
         ):(
-            <Box display="flex" flexDirection="column" width="100%" margin="8px 0 8px 8px" gap="8px">
-                <Box display="flex" flexDirection="column" bgcolor="white" borderRadius="10px" alignItems="center">
-                    <Link href="/home" color="#000" sx={{textDecoration:"none"}}>
-                        <Box display="flex" margin="8px" width="25px" padding="10px" height="25px">
-                            <HiHome size="25px"/>
-                        </Box>
-                    </Link>
-                    <Link href="/search" color="#000" sx={{textDecoration:"none"}}>
-                        <Box display="flex" margin="8px" gap="1px">
-                            <Box display="flex" width="25px" height="25px" padding="10px">
-                                <HiSearch size="25px"/>
-                            </Box>
-                        </Box>
-                    </Link>
+            <Box sx={mainBox}>
+                <Box sx={{...homeBox, alignItems:"center"}}>
+                    <SmallHeaderButtons link="home" icon={<HiHome size="25px"/>}/>
+                    <SmallHeaderButtons link="search" icon={<HiSearch size="25px"/>}/>
                 </Box>
 
-                <Box display="flex" flexDirection="column" bgcolor="white" borderRadius="10px" overflow="auto">
+                <Box sx={libraryBox}>
                     <Box display="flex" justifyContent="center">
                         <ButtonBase disableRipple sx={{gap:"1px", margin:"8px"}} onClick={leftToggle}>
                             <Box display="flex" width="25px" height="25px" padding="10px">
@@ -237,9 +284,9 @@ const Left=({ toggleLeftWidth, toggleMaxWidth })=>{
                             {data.map((item, index) => (
                                 <Button key={index} disableRipple sx={listButton}>
                                     {index === 1 ? (
-                                        <img src={`../../assets/olivia.jpeg`} width="56px" height="56px" style={{ objectFit: 'cover', borderRadius: "10px" }} alt="olivia"/>
+                                        <Image src="../../assets/olivia.jpeg" alt="olivia" />
                                     ) : (
-                                        <img src={`../../assets/liked.png`} width="56px" height="56px" style={{ borderRadius: "10px"}} alt="liked" />
+                                        <Image src="../../assets/liked.png" alt="liked" />
                                     )}
                                 </Button>
                             ))}
